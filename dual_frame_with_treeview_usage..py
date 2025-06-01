725c643ffe28032b3ae9caac4ce8ec413731b430
Khian Calderon <ckhianvictory@gmail.com>
2025-06-01 22:56:16 +0800

    Added Treeview usage with Dual Frame

---
+++ dual_frame_with_treeview_usage..py
@@ -0,0 +1,54 @@
+from tkinter import *
+import customtkinter as ctk
+from dual_frame import create_dual_frame
+from treeview import create_treeview
+
+# Size templates
+app = ctk.CTk()
+screen_width = app.winfo_screenwidth()
+screen_height = app.winfo_screenheight()
+app.geometry(f"{screen_width}x{screen_height}+0+0")
+app.update()
+app.state("zoomed")
+
+# Sample column and data
+sample_columns = ["Name", "Age", "Gender", "Address"]
+sample_data = [
+    ("Alice", 17, "Female", "Brook Street"),
+    ("John", 22, "Male", "Maple Avenue"),
+    ("Sophia", 19, "Female", "Elm Street"),
+    ("Liam", 25, "Male", "Oak Drive"),
+    ("Emma", 21, "Female", "Pine Road"),
+    ("Noah", 18, "Male", "Cedar Lane"),
+    ("Olivia", 20, "Female", "Main Street"),
+    ("James", 23, "Male", "Park Avenue"),
+    ("Isabella", 24, "Female", "Sunset Boulevard"),
+    ("Lucas", 26, "Male", "Highland Road"),
+    ("Mia", 22, "Female", "Riverside Drive"),
+    ("Benjamin", 19, "Male", "Lakeview Street"),
+    ("Amelia", 18, "Female", "Hilltop Road"),
+    ("Elijah", 21, "Male", "Birch Avenue"),
+    ("Charlotte", 23, "Female", "Forest Street"),
+    ("Logan", 20, "Male", "Chestnut Drive"),
+    ("Ava", 25, "Female", "Valley Road"),
+    ("Ethan", 24, "Male", "Hawthorn Street"),
+    ("Harper", 22, "Female", "Willow Way"),
+    ("Daniel", 26, "Male", "Magnolia Lane")
+]
+
+# Content of the frames
+def sample_page(parent):
+    frame = ctk.CTkFrame(parent, fg_color="transparent", corner_radius=0)
+    create_treeview(frame, columns=sample_columns, data=sample_data, col_width=700, alternating_data_bg=False).pack(fill = BOTH)
+    return frame
+
+frame_content = {
+    "Home": sample_page
+}
+
+# Make sure the background color are dark for better UI
+# Either of the frame_content or frame_content_no_divider should work
+dual_frame = create_dual_frame(app, frame_content, left_frame_bg_color="#000a35", right_frame_bg_color="#001545", button_hover_color="#1b285f")
+dual_frame.pack(expand = True, fill = BOTH)
+
+app.mainloop()
