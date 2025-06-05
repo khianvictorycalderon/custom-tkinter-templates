f760dd93396a6292b9615df546d9da81537782db
Khian Calderon <ckhianvictory@gmail.com>
2025-06-05 09:23:54 +0800

    Updated Editable List Usage

--- editable_list_usage.py
+++ editable_list_usage.py
@@ -1,27 +1,32 @@
 from tkinter import *
 import customtkinter as ctk
-from editable_list import create_editable_list, ArrayVar
+from editable_list import create_editable_list
 
-# Size templates
 app = ctk.CTk()
 screen_width = app.winfo_screenwidth()
 screen_height = app.winfo_screenheight()
 app.geometry(f"{screen_width}x{screen_height}+0+0")
-app.update()
 app.state("zoomed")
 
-sample_list = ArrayVar(value=[
-    ["John", "Doe"],
-    ["Jake", "Zyrus"],
-    ["Paul", "Walker"]
-])
+columns = {
+    "Name": "text",
+    "Last Name": "text",
+    "Address": "text",
+    "Age": "number_positive",
+    "Weight": "number",
+    "Department": ["Department 1", "Department 2"]
+}
 
-headers = ["Name", "Last Name", "Address 1", "Address 2", "Address 3"]
+sample_list = [
+    ["John", "Doe", "Street 123", "25", "70", "Department 1"],
+    ["Jake", "Zyrus", "Street 456", "30", "80", "Department 2"]
+]
 
-def my_save_callback(data):
-    for row in data:
-        print(row, ",")
+def my_save_callback(_):
+    print() # New Empty Line
+    for row in sample_list:
+        print(row)
 
-create_editable_list(app, headers=headers, data_var=sample_list, column_per_rows=5, on_save=my_save_callback).pack(expand=True, fill=BOTH)
+create_editable_list(app, columns=columns, data_list=sample_list, on_save=my_save_callback).pack(expand=True, fill=BOTH)
 
 app.mainloop()
